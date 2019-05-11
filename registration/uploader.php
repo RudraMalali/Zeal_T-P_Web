ebc0d84c018160a8e3fc6399e2ebf5194875e8a3
Nikhil Nimbalkar <47498834+nikhilnimbalkar1@users.noreply.github.com>
2019-05-11 11:45:50 +0530

    data input from form
    Check for form validation on same page and files upload.

--- registration/uploader.php
+++ registration/uploader.php
@@ -1,5 +1,38 @@
 <?php
 if(isset($_POST['submit'])){
+$con=new MongoClient();
+$datab=$con->Zeal_TP;
+$collection=$con->Students;
+
+$ftypeerr=$emptfielderr=$emailerr=" ";
+
+	$f_name=$_POST['full_name'];
+	$email=$_POST['email'];
+	$institute=$_POST['institute_name'];
+	$dept=$_POST['department'];
+	$rollno=$_POST['roll_no'];
+	$contactno=$_POST['contact_no'];
+
+	if(empty($f_name)||empty($email)||empty($institute)||empty($dept)||empty($rollno)||empty($contactno))
+	{
+		$emptfielderr="All details are mandatory!Please fill the complete form!";
+	}
+
+	if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+  	$emailErr = "Invalid email format!Please check your Email-Id!"; 
+	} 
+
+	$document = array( 
+		"name" =>$f_name, 
+		"email" => $email, 
+		"institute" =>$institute,
+		"department" =>$dept,
+		"rollno" =>$rollno,
+		"contact_no"=>$contactno
+	 );
+
+	 $collection->insert($document);
+
 	$file = $_FILES['file_cv'];
 	
 	$fileName = $_FILES['file_cv']['name'];
